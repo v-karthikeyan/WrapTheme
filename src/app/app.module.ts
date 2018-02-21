2f83fcb1def77383ab5bcbc78a2d395acaf839d8
Malatesh Patil <malatesh.patil@toolagen.com>
2018-02-21 20:58:26 +0530

    Codetheme integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import {MenuItems} from './menu-items';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [MenuItems],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
